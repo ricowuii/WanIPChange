eca91df5aafc8620fcd775c21963015cd49ec4c5
ricowuii <ricowuii@126.com>
2025-01-04 21:34:31 +0800

    add all wan ip changed
    所有IPv4和IPv6变化后都会发送IYUU通知

--- IPV6_change_informer.sh
+++ IPV6_change_informer.sh
@@ -6,42 +6,50 @@ script_dir=$(dirname $0)
 #导入发送IYUU函数
 source $script_dir/function
 
-
-#ipv6网口名称
-network_wan="ovs_eth0"
 #iyuu标题
-iyuu_text="DS918WANIPChanged"
+iyuu_text="$(uname -n)WANIPChanged"
 #iyuu token
 iyuu_token="IYUU57489Te561fae2c57115d2c158d0438a6df163453b8b51"
-#存储IPv6地址的文件
+#存储IP地址的文件
 wan_ip="$script_dir/wan_ip.txt"
 #脚本执行log文件
 exc_log="$script_dir/check_wan_ip.log"
-#如果只需要获取WAN口的IPv4地址，就把获取WAN口IPv6地址的相关命令删掉
-#只需要获取WAN口的IPv6地址的同理
-# 获取当前的 WAN IPv6 地址
-current_ipv6=$(ip -6 addr show $network_wan | grep -oE 'inet6 [0-9a-fA-F:]+(/[0-9]+)?' | awk '{print $2}'|head -n 1)
-# 检查 wan_ip.txt 文件是否存在，如果不存在则创建文件并发送初始邮件
-if [ ! -f $wan_ip ]; then
-echo "ipv6_wan: $current_ipv6" > $wan_ip
 
-# 发送初始邮件通知
-#echo -e "Subject: OpenWRT WAN IP\n\nFirst running script，WAN IPv4 address: $current_ipv4\nWAN IPv6 address: $current_ipv6_wan \nLAN IPv6 address:$current_ipv6_lan"| msmtp -a default 18918818857@189.cn
-echo "Script executed at $(date)">> $exc_log
+
+# 调用get_global_ips函数，获取所有公网IP
+current_ip="$(get_global_ips)"
+# 检查 wan_ip.txt 文件是否存在，如果不存在则创建文件并发送IYUU通知
+if [ ! -f $wan_ip ]; then
+echo "$current_ip" > $wan_ip
+# 设置IYUU通知内容
+iyuu_desp=$(echo -e "$(date) \n \
+WAN IPs are: \n  \
+$current_ip")
+# 发送IYUU通知
+response=$(send_iyuu "$iyuu_text" "$iyuu_desp" "$iyuu_token")
+# 记录log
+echo "$(date) IPs are written in file $wan_ip">> $exc_log
+echo "$(date) $response">> $exc_log
 fi
+
 # 读取已保存的 IP 地址
 saved_ip=$(cat $wan_ip)
-# 如果当前的 IPv4 或 IPv6 地址与保存的不一致，发送邮件通知并更新记录
-if [ "ipv6_wan: $current_ipv6" != "$saved_ip" ]; then
-#echo -e "Subject: OpenWRT WAN IP Changed\n\nWAN IPv4 changed to: $current_ipv4\nWAN IPv6 changed to : $current_ipv6_wan\nLAN IPv6 changed to : $current_ipv6_lan\n Former IP address: $saved_ip" | msmtp -a default 18918818857@189.cn
-iyuu_desp="WAN IPv6 changed to : $current_ipv6 \
-Former IP address: $saved_ip"
+# 如果当前的IP地址与保存的不一致，发送IYUU并更新记录
+if [ "$current_ip" != "$saved_ip" ]; then
+#设置IYUU通知内容
+iyuu_desp=$(echo -e "$(date) \n \
+WAN IPs changed to :\n \
+$current_ip \n \
+Former IPs address: \n \
+$saved_ip")
+#发送IYUU通知
 response=$(send_iyuu "$iyuu_text" "$iyuu_desp" "$iyuu_token")
-# 更新 IP 记录
-echo "ipv6_wan: $current_ipv6" > $wan_ip
+# 更新IP记录文件为最新的IP地址
+echo "$current_ip6" > $wan_ip
+# 更新事件记录log
 echo "$(date) Script executed at $(date) WAN IP has changed">> $exc_log
 echo "$(date) $response">> $exc_log
 else
-echo "$(date) WAN IP does not changed.WAN IPV6 address:$current_ipv6">> $exc_log
+# 未更新，执行记录记录Log
+echo "$(date) WAN IP does not changed.WAN IPs address:$current_ip">> $exc_log
 fi
-
